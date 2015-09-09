bc894e6e35653bded08e4968a7efbda6c25b521d
Aaron Llanos <aaronllanos@gmail.com>
2015-09-09 05:13:43 -0500

    angularjs

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aaronLlanosgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the aaronLlanosgithubioApp
+ */
+angular.module('aaronLlanosgithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
